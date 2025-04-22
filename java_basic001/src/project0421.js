3b0b74301b0d9ec41222f6fbd6283ad81048f3f6
Bae-junsung <bjs95526852@daum.net>
2025-04-22 17:15:36 +0900

    +1

--- java_basic001/src/project0421.js
+++ java_basic001/src/project0421.js
@@ -25,19 +25,3 @@ class Exam61 {
 }
 const exam61 = new Exam61();
 exam61.sen();
-
-/*
-//버스 정류장
-class Exam68 {
-    constructor(time = prompt("버스 시간을 입력하세요:")) {
-        this.time = time;
-    }
-    bus(){
-    for(int i = 0; i < time.length; i++){
-        time =
-     }
-    }
-}
-const exam68 = new Exam68();
-exam68.bus();
-*/
