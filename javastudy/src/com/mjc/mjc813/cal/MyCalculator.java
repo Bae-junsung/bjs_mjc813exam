c68f08766d5c34d644a7516d777fac93d34d20f9
Bae-junsung <bjs95526852@daum.net>
2025-04-30 18:12:39 +0900

    Update MyCalculator.java

--- javastudy/src/com/mjc/mjc813/cal/MyCalculator.java
+++ javastudy/src/com/mjc/mjc813/cal/MyCalculator.java
@@ -16,11 +16,11 @@ public class MyCalculator {
         int a1 = 10;
         int b1 = 30;
         int n1 = mCal.myAdd(a1, b1);
-        System.out.println("두 수("+ a1 + "," + b1 + ")의 값은" + n1 + "입니다.");
+        System.out.println("두 수("+ a1 + "," + b1 + ")의 값은 " + n1 + " 입니다.");
 
         long a2 = 10;
         long b2 = 30;
         long n2 = mCal.myAdd(a2, b2);
-        System.out.println("두 수("+ a2 + "," + b2 + ")의 값은" + n2 + "입니다.");
+        System.out.println("두 수("+ a2 + "," + b2 + ")의 값은 " + n2 + " 입니다.");
     }
 }
