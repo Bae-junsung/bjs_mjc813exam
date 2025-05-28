c1bb37d10d1c757dffb7ceb6db493f9cc35b688a
Bae-junsung <bjs95526852@daum.net>
2025-05-28 21:33:21 +0900

    +팀원들 테스트

---
+++ bjs_Information/src/test/java/com/bjs/TestMjcValidCheck.java
@@ -0,0 +1,26 @@
+package com.bjs;
+
+import com.mjc813.ksm.lib.MjcValidCheck;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+public class TestMjcValidCheck {
+    @Test
+    public void MjcValidCheck() {
+        MjcValidCheck mvc = new MjcValidCheck();
+        //전화번호
+        assertThat(mvc.isValidPhoneNumber(null)).isEqualTo(false);
+        assertThat(mvc.isValidPhoneNumber("")).isEqualTo(false);
+        assertThat(mvc.isValidPhoneNumber("010-39a2-0203")).isEqualTo(false);
+        assertThat(mvc.isValidPhoneNumber("010-1111-2222")).isEqualTo(true);
+        //우편번호
+        assertThat(mvc.isValidZipNumber(null)).isEqualTo(false);
+        assertThat(mvc.isValidZipNumber("")).isEqualTo(false);
+        assertThat(mvc.isValidZipNumber("0982")).isEqualTo(false);
+        assertThat(mvc.isValidZipNumber("12345")).isEqualTo(true);
+        //이메일
+        assertThat(mvc.isValidEmail(null)).isEqualTo(false);
+        assertThat(mvc.isValidEmail("")).isEqualTo(false);
+        assertThat(mvc.isValidEmail("@daum.")).isEqualTo(false);
+        assertThat(mvc.isValidEmail("abc@naver.com")).isEqualTo(true);
+    }
+}
