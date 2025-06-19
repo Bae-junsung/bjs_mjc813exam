5c9a54f0042c732fe703796a81ac0a0e065441f9
Bae-junsung <bjs95526852@daum.net>
2025-06-19 16:29:48 +0900

    +1권 복습(6장)

---
+++ javastudy/src/review1/CE.java
@@ -0,0 +1,57 @@
+package review1;
+
+public class CE {
+    //  String company="현대";
+//   String model;
+//   int maxSpeed;
+//   //*생성자(충돌방지!!)
+//   public CE(String model) {
+//       this.model=model;
+//   }
+//   public CE(String model, int maxSpeed){
+//       this.model=model;
+//       this.maxSpeed=maxSpeed;
+//   }
+//   int plus(int x,int y){
+//       int result = x + y;
+//       return result;
+//   }
+//  //***배열 생성자***
+//  int sum(int ... values){
+//      int sum = 0;
+//      for(int i=0; i<values.length; i++){
+//          sum += values[i];
+//      }
+//      return sum;
+//  }
+//    //메소드 밖(return쓰기!!)
+//    public int CE(int width){
+//        return width*width;
+//    }
+//    public int CE(int width, int height){
+//        return width*height;
+//    }
+//   //정적 멤버-> static!!
+//   static double pi = 3.14;
+//   //final(한번 쓰면 수정 불가)
+//   final String name="홍길동";
+//   final String ssn;
+//   int age;
+//   public CE(String ssn, int age){
+//       this.age=age;
+//       this.ssn = ssn;
+//   }
+
+//   //***Getter, Setter***
+//   private int speed;
+//   public void setValue(int speed){ //Set은 값 생성
+//       if(speed<0){
+//           this.speed=0;
+//       }else{
+//           this.speed=speed;
+//       }
+//   }
+//   public int getSpeed() { //Get은 값 출력
+//       return speed;
+//   }
+}
---
+++ javastudy/src/review1/ClassExample.java
@@ -0,0 +1,55 @@
+package review1;
+
+public class ClassExample {
+    public static <Tire, Bus> void main(String[] args){
+//        String a = new String("학생1");
+//        System.out.println("A: " + a);
+//
+//        String b = new String("학생2");
+//        System.out.println("B: " + b);
+//      CE car = new CE("현대","그랜저",300);
+//      System.out.println(car.company+car.model+car.maxSpeed);
+//       CE car = new CE("기아","K7",400);
+//       System.out.println(car.company+car.model+car.maxSpeed);
+//        CE c1 = new CE("현대");
+//        System.out.println(c1.company);
+//        System.out.println(c1.model);
+//        CE c2 = new CE("기아", 500);
+//        System.out.println(c2.company);
+//        System.out.println(c2.model);
+//        System.out.println(c2.maxSpeed);
+//     CE ce = new CE();
+//     double a = ce.plus(5,6);
+//     System.out.println(a);
+//       //**배열 원소 합**
+//       CE num1 = new CE();
+//       int result1 = num1.sum(1,2,3,4,5,6);
+//       System.out.println(result1);
+//        CE r1 = new CE();
+//        System.out.println(r1.CE(4));
+//        CE r2 = new CE();
+//        System.out.println(r2.CE(2,4));
+//        //정적 멤버-> static!!
+//        CE result = new CE();
+//        double result1 = 10*10* CE.pi;
+//        System.out.println(result1);
+//       //final(한번 쓰면 수정 불가)
+//       CE a = new CE("123456-7890", 20);
+//       System.out.println(a.name);
+//       System.out.println(a.ssn);
+//       System.out.println(a.age);
+//       a.age = 12; //final 안 붙은거는 수정 가능!
+//       System.out.println(a.age);
+
+//       //***Getter, Setter*** - 무결성 깨짐 방지(예시로 자동차 속력은 음수 X)
+//       CE car = new CE();
+//       car.setValue(-1);
+//       System.out.println(car.getSpeed());
+//       car.setValue(0);
+//       System.out.println(car.getSpeed());
+//       car.setValue(100);
+//       System.out.println(car.getSpeed());
+
+        //싱글톤은 하나의 객체만 사용!->getInstance()
+    }
+}
