684b52ac6f6326f76957cf09798872e1482c2a31
Bae-junsung <bjs95526852@daum.net>
2025-06-20 17:03:47 +0900

    +2권 복습(모듈 미완)

---
+++ javastudy/src/review2/ME.java
@@ -0,0 +1,40 @@
+package review2;
+//equlas(오버라이딩 암기? 이해?)
+//instance of->어떤 클래스의 타입인지를 확인하는 연산자
+
+public class ME {
+//    public String c;
+//    public ME(String c){
+//        this.c=c;
+//    }
+//    @Override
+//    public boolean equals(Object obj){
+//        if(obj instanceof ME target){ //instance of->어떤 클래스의 타입인지를 확인하는 연산자
+//            if(c.equals(target.c)){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
+//   public String name;
+//   public int age;
+//   public ME(int age, String name){
+//       this.age=age;
+//       this.name=name;
+//   }
+//   @Override
+//   public int hashCode(){
+//       int hashCode = age + name.hashCode(); //hashCode는 문자열의 정수값!!
+//       return hashCode;
+//   }
+//   @Override
+//   public boolean equals(Object obj){
+//       if(obj instanceof ME target){
+//           if(age == target.age && name == target.name){
+//               return true;
+//           }
+//       }
+//       return false;
+//   }
+}
---
+++ javastudy/src/review2/ModuleExample.java
@@ -0,0 +1,27 @@
+package review2;
+
+public class ModuleExample {
+    public static void main(String[] args) {
+//       ME c1 = new ME("blue");
+//       ME c2 = new ME("blue");
+//       ME c3 = new ME("red");
+//       if (c1.equals(c2)) { //equals 사용
+//           System.out.println("같음");
+//       } else {
+//           System.out.println("다름");
+//       }
+//       if (c1.equals(c3)) {
+//           System.out.println("같음");
+//       } else {
+//           System.out.println("다름");
+//       }
+
+//        ME o1 = new ME(10,"홍길동");
+//        ME o2 = new ME(10,"홍길똥");
+//        if(o1.equals(o2)){
+//            System.out.println("일치");
+//        }else{
+//            System.out.println("불일치");
+//        }
+    }
+}
