b71f5b58ef323ffd0bc5470acb8f17fc59155946
Bae-junsung <bjs95526852@daum.net>
2025-06-24 09:35:09 +0900

    +제너릭,스레드(완전 이해x)

---
+++ javastudy/src/review2/GE.java
@@ -0,0 +1,20 @@
+package review2;
+
+public class GE<T>{
+//    public T content; //타입 파라미터 선언!!(암기)
+//    //동등한지 비교
+//    public boolean equals(GE<T> other) {
+//        boolean result = content.equals(other.content);
+//        return result;
+//    }
+    
+//    //제너릭 메소드
+//    private T t; //필드선언
+//    public void setContent(T t) {
+//        this.t = t;
+//    }
+//    // Getter for content (retrieves the content)
+//    public T getContent() {
+//        return this.t;
+//    }
+}
---
+++ javastudy/src/review2/GenericExample.java
@@ -0,0 +1,44 @@
+package review2;
+
+import unit13.Box;
+import unit13.Product;
+
+public class GenericExample {
+//    //제한된 타입 파라미터
+//    public static<T extends Number> boolean compare(T t1,T t2){
+//        return (t1 != t2);
+//    }
+    public static void main(String[] args){
+//        //제너릭 String
+//        GE<String> box1 = new GE<>();
+//        box1.content = "안녕하세요";
+//        System.out.println(box1.content);
+//        //제너릭 Integer
+//        GE<Integer> box2 = new GE<>();
+//        box2.content = 10;
+//        System.out.println(box2.content);
+
+//       //동등한지 비교
+//       GE<String> box1 = new GE<>();
+//       box1.content = "100";
+//       GE<String> box2 = new GE<>();
+//       box2.content = "100";
+//       boolean result1 = box1.equals(box2);
+//       System.out.println(result1);
+
+//       //제너렉 메소드
+//       GE<Integer> box1 = new GE<>();
+//       box1.setContent(100);
+//       System.out.println(box1.getContent());
+//       GE<String> box2 = new GE<>();
+//       box2.setContent("홍길동");
+//       System.out.println(box2.getContent());
+
+//        //제한된 타입 파라미터
+//        boolean r1 = compare(10, 20);
+//        System.out.println(r1);
+//        boolean r2 = compare(10,10);
+//        System.out.println(r2);
+
+    }
+}
